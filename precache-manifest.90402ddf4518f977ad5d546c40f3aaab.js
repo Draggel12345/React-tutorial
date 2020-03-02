811b7c61ebca02886dfd3b9f24334f63c60e3497
Draggel12345 <anton.edholm@hotmail.com>
2020-03-02 15:51:27 +0100

    Updates

---
+++ precache-manifest.90402ddf4518f977ad5d546c40f3aaab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25bc2ccc5ef4a605e786fe1105f04151",
+    "url": "/React-tutorial/index.html"
+  },
+  {
+    "revision": "766f80f9530e2bdd7491",
+    "url": "/React-tutorial/static/css/main.e54c786b.chunk.css"
+  },
+  {
+    "revision": "109512087cd39772a75c",
+    "url": "/React-tutorial/static/js/2.3fd44583.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-tutorial/static/js/2.3fd44583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "766f80f9530e2bdd7491",
+    "url": "/React-tutorial/static/js/main.3d42a468.chunk.js"
+  },
+  {
+    "revision": "0b71099f3e6c365290b7",
+    "url": "/React-tutorial/static/js/runtime-main.40f1f094.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-tutorial/precache-manifest.90402ddf4518f977ad5d546c40f3aaab.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-tutorial/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
